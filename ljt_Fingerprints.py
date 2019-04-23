fd6715001cef030304f459376e0e85eecc02ae47
ljt15 <ljt15@inf.ufpr.br>
2019-04-22 21:15:08 -0300

    Preparando para calcular o bloco com os gradientes
    Signed-off-by: ljt15 <ljt15@inf.ufpr.br>

--- ljt_Fingerprints.py
+++ ljt_Fingerprints.py
@@ -37,6 +37,15 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 path = 'Rindex28/'
 #path = 'Rindex28-type/'
 
+def plot_images(self,imgs,titles): #by bruno meyer
+        fig = plt.figure(figsize=(12, 3))
+        for i, a in enumerate(imgs):
+            ax = fig.add_subplot(1, len(imgs), i + 1)
+            ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+            ax.set_title(titles[i], fontsize=10)
+            ax.set_xticks([])
+            ax.set_yticks([])
+
 def main():
 	if path == 'Rindex28/' or path == 'Lindex101/' :
 		image_paths = [os.path.join(path, f) for f in os.listdir(path) if  f.endswith('.raw')]
@@ -47,7 +56,7 @@ def main():
 		#print(image_paths)
 		#print(os.listdir(path))
 		print("Loading database: "+ path)
-		for image_path in image_paths:
+		for image_path in image_paths:	
 			#print(image_path)
 
 			A = np.fromfile(image_path, dtype='int8', sep="")
@@ -61,6 +70,7 @@ def main():
 
 
 
+
 			#plt.imshow(A, cmap="gray")
 			#plt.show()
 
@@ -69,11 +79,15 @@ def main():
 
 	processedImages = []
 	#enhancing image
+
+	alpha = 150
+	gamma = 95 #default
+	#gamma = 70 
 	for image in images:
 		print(np.mean(image))
 		print(np.std(image))
 		print(type(image))
-		enhanced = 150 + 95 * (image - np.mean(image)) / np.std(image)
+		enhanced = alpha + gamma * (image - np.mean(image)) / np.std(image)
 		print(type(enhanced))
 		#plt.imshow(enhanced, cmap="gray")
 		#plt.show()
@@ -82,12 +96,43 @@ def main():
 
 
 
+
+
 	for image in processedImages:
-		image = ndimage.median_filter(image, size=(5,5))
-		plt.imshow(image, cmap="gray")
+		image = ndimage.median_filter(image, size=(5,5)) #tamanho do filtro de mediana
+		
+
+	for image in processedImages: #parece SUSPEITO! REVER!!!!
+
+		sobelX = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=3)#tamanho dos filtros de Sobel
+		sobelY = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=3)
+
+		hipoteSobel = np.sqrt(np.square(sobelX) + np.square(sobelY))
+
+		##trata divisoes por 0. REVER!!
+		angleSobel = np.arctan(np.divide(sobelY,sobelX,out=np.zeros_like(sobelY), where=sobelX!=0))
+		#plt.imshow(sobelX, cmap="gray")
+		#plt.show()
+		#plt.imshow(sobelY, cmap="gray")
+		#plt.show()
+		#plt.imshow(hipoteSobel, cmap="gray")
+		#plt.show()
+		#plt.imshow(angleSobel, cmap="gray")
+		#plt.show()
+
+		alphaX = np.square(hipoteSobel)*np.cos(2*angleSobel)
+		alphaY = np.square(hipoteSobel)*np.sin(2*angleSobel)
+		plt.imshow(alphaX, cmap="gray")
+		plt.show()
+		plt.imshow(alphaY, cmap="gray")
 		plt.show()
 
+		#plot_images()
 
+
+
+
+#USAR NP.ARCTAN2 no slide 9!!!
 
 
 
